9859a8c0b8c793c0adc3a89991076371f8fe9d93
Andres-Quetz <2005440@utivieramaya.edu.mx>
2021-11-02 15:42:08 -0500

    tarea

---
+++ vueform/src/views/About.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-form v-model="valid">
+        <v-container>
+            <v-row>
+                <v-col cols="12" ad="4">
+                    <v-text-field
+                        v-model="firstname"
+                        :rules="nameRules"
+                        :counter="10"
+                        label="First name:"
+                        required
+                    >
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" md="4">
+                    <v-text-field
+                        v-model="email"
+                        :rules="mailRules"
+                        label="E-mail"
+                        required
+                    >
+                    </v-text-field>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-form>
+</template>
+<script>
+export default {
+    data: () => ({
+        valid: false,
+        firstname: '',
+        email: '',
+        nameRules: [
+            (v) => !!v || 'Name is required',
+            (v) => v.length <= 10 || 'name must be less than 10 charact',
+        ],
+        mailRules: [
+            (v) => !!v || 'Email is required',
+            (v) => /.+@+.+/.test(v) || 'E-mail must be valid',
+        ],
+    }),
+};
+</script>
