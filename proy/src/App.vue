3055072aad541a201d7310114d03cb4adeee5bec
Andres-Quetz <2005440@utivieramaya.edu.mx>
2021-11-02 21:28:11 -0500

    login

---
+++ proy/src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <v-app>
+        <v-navigation-drawer app> </v-navigation-drawer>
+
+        <v-app-bar app>
+
+            <router-link to="/">Home</router-link> |
+
+            <router-link to="/about">Acerca de </router-link>
+        </v-app-bar>
+
+        <v-main>
+            <v-container fluid>
+                <router-view></router-view>
+            </v-container>
+        </v-main>
+
+    </v-app>
+    
+</template>
---
+++ proy/src/views/About.vue
@@ -0,0 +1,68 @@
+<template>
+<v-sheet
+ 
+  elevation="24"
+
+ 
+>
+    <v-form v-model="valid">
+        <v-container>
+            <v-row>
+                <v-col cols="12" ad="4">
+                    <v-text-field
+                        v-model="username"
+                        :rules="nameRules"
+                        label="User name:"
+                        required
+                    >
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" md="4">
+                    <v-text-field
+                        v-model="password"
+                        :rules="passRules"
+                        label="Password"
+                        required
+                    >
+                    </v-text-field>
+                </v-col>
+
+                <v-col cols="1" md="12">
+                    <v-btn
+                        color="secondary"
+                        elevation="24"
+                     >Login </v-btn>
+
+                     <v-btn
+                        color="secondary"
+                        elevation="24"
+                     >Forgot? </v-btn>
+                </v-col>
+                <v-col >
+                    
+                </v-col>
+                
+                
+            </v-row>
+        </v-container>
+    </v-form>
+  </v-sheet>
+    
+</template>
+<script>
+export default {
+    data: () => ({
+        valid: false,
+        username: '',
+        password: '',
+        nameRules: [
+            (v) => !!v || 'Username is required',
+
+        ],
+        passRules: [
+            (v) => !!v || 'password is required',
+            
+        ],
+    }),
+};
+</script>
