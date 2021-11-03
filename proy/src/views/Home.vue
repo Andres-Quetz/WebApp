0b35363a8723ea17cf29bd24f03a7d5d3a294ba4
Andres-Quetz <2005440@utivieramaya.edu.mx>
2021-11-02 21:27:47 -0500

    log in

---
+++ proy/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-bottom-navigation
+    :value="value"
+    color="red"
+  >
+    <v-btn>
+     
+    </v-btn>
+
+     <v-btn>
+     
+    </v-btn>
+
+    <v-btn>
+    
+    </v-btn>
+
+    <v-btn>
+      
+    <router-link to="#">Sing up </router-link>
+      
+    </v-btn>
+
+     <v-btn>
+    <router-link to="/about">Login </router-link>
+    </v-btn>
+    
+  </v-bottom-navigation>
+</template>
