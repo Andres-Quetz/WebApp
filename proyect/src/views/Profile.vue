62ac28c45b7f7d5c27cc5db0fb72b1c67931416b
Andres-Quetz <2005440@utivieramaya.edu.mx>
2021-12-05 22:19:09 -0500

    proyect

--- proyect/src/views/Profile.vue
+++ proyect/src/views/Profile.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <v-toolbar
+    color="white"
+  dark
+>
+<v-btn to="/sale" style="color:black" color="transparent">Sale</v-btn>
+
+</v-toolbar>
 </template>
---
+++ proyect/src/views/Settings.vue
@@ -0,0 +1,28 @@
+
+<template fluid>
+
+  <v-container>
+ 
+   <v-row>
+     <v-col cols="4" md="2" sm="5" xs="5">
+       <v-card
+            class="travel"
+            max-height="150"
+            max-width="120"
+            color="transparent"
+          >
+            <v-card-text class="text-xs-center">
+              <div class="category">Security</div>
+              <v-btn to="/login"  color="transparent"> <v-icon>security</v-icon>
+      </v-btn>
+            </v-card-text>
+          </v-card>
+ 
+     </v-col>
+   </v-row>
+  </v-container>
+  
+</template>
+
+
+
